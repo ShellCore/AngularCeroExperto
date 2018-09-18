41a4fc97f9baa0eb95db250ac4c44b9906b47ac5
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-17 21:52:49 -0500

    Se realizó el maquetado de los datos del héroe seleccionado

--- 02-spa/src/app/components/heroe/heroe.component.ts
+++ 02-spa/src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Heroe, HeroesService } from '../../services/heroes.service';
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html'
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html'
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+    heroe: Heroe;
 
-  ngOnInit() {
-  }
+    constructor(
+        private _heroesService: HeroesService,
+        private activatedRoute: ActivatedRoute
+    ) {
+        this.activatedRoute.params.subscribe(params => {
+            this.heroe = this._heroesService.getHeroe(params['id']);
+            console.log(this.heroe);
+        });
+    }
+
+    ngOnInit() {
+    }
 
 }
