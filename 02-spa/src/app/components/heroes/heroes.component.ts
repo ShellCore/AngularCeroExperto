120f3c09952c1d2a883589f22bcbf8f59c0115de
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-17 21:33:04 -0500

    Se agregó redireccionamiento al tocar el botón "Ver más" de cada héroe

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-heroes',
@@ -10,7 +11,8 @@ export class HeroesComponent implements OnInit {
     heroes: Heroe[] = [];
 
     constructor(
-        private _heroesService: HeroesService
+        private _heroesService: HeroesService,
+        private _router: Router
     ) {
         console.log('Constructor');
     }
@@ -23,5 +25,9 @@ export class HeroesComponent implements OnInit {
     loadHeroes() {
         this.heroes = this._heroesService.getHeroes();
         console.log(this.heroes);
+    }
+
+    verHeroe(id: number) {
+        this._router.navigate(['/heroe', id]);
     }
 }
