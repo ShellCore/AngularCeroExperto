842478ee83a304d934274c714084b992064dc40b
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-19 16:05:15 -0500

    Actualización de los servicios, pasando respuesta por un pipe, y refactorización de código para hacerlo más mantenible

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -10,21 +11,22 @@ export class SpotifyService {
         console.log("Spotify listo!");
     }
 
-    getNewReleases() {
+    getQuery(query: string) {
+        const url = `https://api.spotify.com/v1/${query}`;
         const headers = new HttpHeaders({
-            'Authorization': 'Bearer BQBH1L5mFQn38G6-sB7uU7Qddy58QpFffyEqOcnelZA_fvcJDrLcBwZOFOeDN7zOv5VeQVDkron0gd14eVM'
+            'Authorization': 'Bearer BQC_DbFU9CS0GN4VJSlWKHVFruIQycNPy6mhT449nW5ZIid0DnfTRtDbO14_pLVkrd0r4-VP-WBfnHLyBO8'
         });
 
-        return this.http
-            .get("https://api.spotify.com/v1/browse/new-releases", { headers });
+        return this.http.get(url, {headers});
+    }
+
+    getNewReleases() {
+        return this.getQuery("browse/new-releases?limit=20")
+            .pipe(map(data => data['albums'].items));
     }
 
     getArtist(termino: String) {
-        const headers = new HttpHeaders({
-            'Authorization': 'Bearer BQBH1L5mFQn38G6-sB7uU7Qddy58QpFffyEqOcnelZA_fvcJDrLcBwZOFOeDN7zOv5VeQVDkron0gd14eVM'
-        });
-
-        return this.http
-            .get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&offset=15`, { headers });
+        return this.getQuery(`search?q=${termino}&type=artist&offset=15`)
+            .pipe(map(data => data['artists'].items));
     }
 }
