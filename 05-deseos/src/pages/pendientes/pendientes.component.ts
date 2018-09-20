fefeca50764475f3e0f9e9167112f36ecf34c638
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-20 15:55:30 -0500

    Se modificaron los estilos y generaron las listas básicas para el maquetado de las vistas

--- 05-deseos/src/pages/pendientes/pendientes.component.ts
+++ 05-deseos/src/pages/pendientes/pendientes.component.ts
@@ -1,16 +1,18 @@
 import { Component } from "@angular/core";
 import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../models/lista.model';
 
 @Component({
     selector: 'page-pendientes',
     templateUrl: 'pendientes.component.html'
 })
-
 export class PendientesPage {
 
     constructor(
         public deseosService: DeseosService
-    ) {
-        
+    ) {}
+
+    listaSeleccionada(lista: Lista) {
+        console.log(lista);
     }
 }
