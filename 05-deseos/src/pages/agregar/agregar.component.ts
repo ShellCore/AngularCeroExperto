cb87e6126646f342b1f02c8d4a3606c831e911a9
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-20 17:54:12 -0500

    Se agregó funcionalidad para agregar items a la lista

--- 05-deseos/src/pages/agregar/agregar.component.ts
+++ 05-deseos/src/pages/agregar/agregar.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from '../../models/lista.model';
 import { NavParams } from "ionic-angular";
+import { ListaItem } from '../../models/lista-item.module';
 
 @Component({
     selector: 'page-agregar',
@@ -10,6 +11,7 @@ import { NavParams } from "ionic-angular";
 export class AgregarPage {
 
     lista: Lista;
+    nombreItem: string = '';
 
     constructor(
         public deseosService: DeseosService,
@@ -19,5 +21,15 @@ export class AgregarPage {
         this.lista = new Lista(titulo);
     }
 
+    agregarItem() {
+        if (this.nombreItem.length > 0) {
+            const nuevoItem = new ListaItem(this.nombreItem);
+            this.lista.items.push(nuevoItem);
+            this.nombreItem = '';
+        }
+    }
     
+    actualizarTarea(item: ListaItem) {
+        item.completado = !item.completado;
+    }
 }
