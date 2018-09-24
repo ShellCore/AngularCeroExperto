76177ed36707ee2c4d0012aba9ae5649eb6f39f4
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-24 11:40:31 -0500

    Ejemplo de ciclo de vida de un componente

--- 06-miscelaneos/src/app/components/home/home.component.ts
+++ 06-miscelaneos/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'app-home',
@@ -21,11 +21,36 @@ import { Component, OnInit } from '@angular/core';
   `,
     styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,
+    AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-    constructor() { }
-
-    ngOnInit() {
+    constructor() { 
+        console.log("constructor");
     }
 
+    ngOnInit() {
+        console.log("ngOnInit");
+    }
+
+    ngOnChanges(changes: import("/home/shell/Projects/Personal/Angular/06-miscelaneos/node_modules/@angular/core/src/metadata/lifecycle_hooks").SimpleChanges): void {
+        console.log("ngOnChanges");
+    }
+    ngDoCheck(): void {
+        console.log("ngDoCheck");
+    }
+    ngAfterContentInit(): void {
+        console.log("ngAfterContentInit");
+    }
+    ngAfterContentChecked(): void {
+        console.log("ngAfterContentChecked");
+    }
+    ngAfterViewInit(): void {
+        console.log("ngAfterViewInit");
+    }
+    ngAfterViewChecked(): void {
+        console.log("ngAfterViewChecked");
+    }
+    ngOnDestroy(): void {
+        console.log("ngOnDestroy");
+    }
 }
