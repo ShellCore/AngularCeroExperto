8205926eddce3ab333a073914d0a04ea08665440
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-24 15:24:13 -0500

    Se agregó la referencia a un objeto local, así como la obtención y el llenado predeterminado de los campos

--- 08-formularios/src/app/components/template/template.component.ts
+++ 08-formularios/src/app/components/template/template.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
-  styles: []
+    selector: 'app-template',
+    templateUrl: './template.component.html',
+    styles: []
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent {
 
-  constructor() { }
+    usuario: Object = {
+        nombre: "Cesar",
+        apellido: "Morales",
+        correo: ""
+    }
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    guardar(forma: NgForm) {
+        console.log("ngForm", forma);
+        console.log("Valor", forma.value);
+        console.log("Usuario", this.usuario);
+    }
 }
