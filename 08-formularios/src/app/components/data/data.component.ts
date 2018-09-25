a8e74573bdac035057b887b02414c2f6822bb9e8
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-25 12:19:39 -0500

    Se agregó ejemplo de agrupación de elementos (nombre y apellido)

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -10,13 +10,23 @@ export class DataComponent {
 
     forma: FormGroup;
 
+    usuario: Object = {
+        nombreCompleto: {
+            nombre: "Cesar",
+            apellido: "Morales"
+        },
+        correo: "cmorales@mail.com"
+    }
+
     constructor() {
         this.forma = new FormGroup({
-            'nombre': new FormControl('', [
-                Validators.required,
-                Validators.minLength(3)
-            ]),
-            'apellido': new FormControl('', Validators.required),
+            'nombrecompleto' : new FormGroup({
+                'nombre': new FormControl('', [
+                    Validators.required,
+                    Validators.minLength(3)
+                ]),
+                'apellido': new FormControl('', Validators.required)
+            }),
             'correo': new FormControl('', [
                 Validators.required, 
                 Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
