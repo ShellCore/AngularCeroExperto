90d31629bf7d4848e8ea6f26c7e8f107b0ef1e5c
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-26 12:45:23 -0500

    Se agregó interface con los datos del heroe necesarios para su comunicación

--- 09-heroesapp/src/app/app.module.ts
+++ 09-heroesapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { APP_ROUTING } from './app.routes';
 
@@ -16,6 +17,7 @@ import { HeroeComponent } from './components/heroes/heroe.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpModule,
     APP_ROUTING
   ],
--- 09-heroesapp/src/app/components/heroes/heroe.component.ts
+++ 09-heroesapp/src/app/components/heroes/heroe.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroe.interface';
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styles: []
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html',
+    styles: []
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+    heroe: Heroe = {
+        nombre: "",
+        bio: "",
+        casa: "Marvel"
+    }
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+    }
+
+    guardar() {
+        console.log(this.heroe);
+    }
 }
