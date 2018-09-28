af1a7720c5e7997d7d9306fb2606ab8c7b659293
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-09-28 13:21:36 -0500

    Creación de componentes requeridos

--- 11-movieapp/src/app/app.module.ts
+++ 11-movieapp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
