4ea234436c783bb1c0580aa620322b3b7c3df0c6
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-10-01 17:47:46 -0500

    Se almacena en memoria los marcadores seleccionados

--- 12-mapas/src/app/components/mapa/mapa.component.ts
+++ 12-mapas/src/app/components/mapa/mapa.component.ts
@@ -2,27 +2,33 @@ import { Component, OnInit } from "@angular/core";
 import { Marcador } from "../../classes/marcador.class";
 
 @Component({
-  selector: "app-mapa",
-  templateUrl: "./mapa.component.html",
-  styleUrls: ["./mapa.component.css"]
+    selector: "app-mapa",
+    templateUrl: "./mapa.component.html",
+    styleUrls: ["./mapa.component.css"]
 })
 export class MapaComponent implements OnInit {
-  marcadores: Marcador[] = [];
+    
+    marcadores: Marcador[] = [];
 
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+    lat = 19.399680115042337;
+    lng = -99.1977643023767;
 
-  constructor() {
-    const m1 = new Marcador(51.678418, 7.809007);
+    constructor() {
+        if (localStorage.getItem('marcadores')) {
+            this.marcadores = JSON.parse(localStorage.getItem('marcadores'));
+        }
+    }
 
-    this.marcadores.push(m1);
-  }
+    ngOnInit() { }
 
-  ngOnInit() {}
+    agregarMarcador(evento) {
+        const coords: { lat: number, lng: number } = evento["coords"];
+        const m1 = new Marcador(coords.lat, coords.lng);
+        this.marcadores.push(m1);
+        this.guardarStorage();
+    }
 
-  agregarMarcador(evento) {
-    const coords: {lat: number, lng: number} = evento["coords"];
-    const m1 = new Marcador(coords.lat, coords.lng);
-    this.marcadores.push(m1);
-  }
+    guardarStorage() {
+        localStorage.setItem('marcadores', JSON.stringify(this.marcadores));
+    }
 }
