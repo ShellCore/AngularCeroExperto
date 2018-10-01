13415a6ea428904bae94264bbb2696a04a81b369
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-10-01 17:36:57 -0500

    Se creó evento para generar pines cuando se da click en algún lugar del mapa

---
+++ 12-mapas/src/app/classes/marcador.class.ts
@@ -0,0 +1,11 @@
+export class Marcador {
+
+    constructor(
+        public lat: number,
+        public lng: number,
+        public title: string = "Sin título",
+        public desc: string = "Sin descripción"
+    ) {
+
+    }
+}
--- 12-mapas/src/app/components/mapa/mapa.component.ts
+++ 12-mapas/src/app/components/mapa/mapa.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Marcador } from "../../classes/marcador.class";
 
 @Component({
-  selector: 'app-mapa',
-  templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.css']
+  selector: "app-mapa",
+  templateUrl: "./mapa.component.html",
+  styleUrls: ["./mapa.component.css"]
 })
 export class MapaComponent implements OnInit {
+  marcadores: Marcador[] = [];
 
-    lat: number = 51.678418;
-    lng: number =  7.809007;
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 
-  constructor() { }
+  constructor() {
+    const m1 = new Marcador(51.678418, 7.809007);
 
-  ngOnInit() {
+    this.marcadores.push(m1);
   }
 
+  ngOnInit() {}
+
+  agregarMarcador(evento) {
+    const coords: {lat: number, lng: number} = evento["coords"];
+    const m1 = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(m1);
+  }
 }
