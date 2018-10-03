48c45d7e9bb564b53e3553528e42fa751415c472
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-10-03 17:03:01 -0500

    Se agregó librería Charts.js

--- 16-graficos/src/app/app.module.ts
+++ 16-graficos/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+// Gráficos
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
