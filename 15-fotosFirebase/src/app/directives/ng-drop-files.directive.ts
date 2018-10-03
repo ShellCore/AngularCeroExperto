8677ad2f12ba81520460dcf1590e230ccfdbc4db
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-10-03 14:52:43 -0500

    Se agregaron validaciones para tipo de archivos y si ya existen en la lista

--- 15-fotosFirebase/src/app/directives/ng-drop-files.directive.ts
+++ 15-fotosFirebase/src/app/directives/ng-drop-files.directive.ts
@@ -14,11 +14,38 @@ export class NgDropFilesDirective {
     @HostListener('dragover', ['$event'])
     public onDragEnter(event: any) {
         this.mouseSobre.emit(true);
+        this._preventStop(event);
     }
 
     @HostListener('dragleave', ['$event'])
     public onDragLeave(event: any) {
         this.mouseSobre.emit(false);
+    }
+
+    @HostListener('drop', ['$event'])
+    public onDrop(event: any) {
+        const transfer = this._getTransference(event);
+        if (!transfer) {
+            return;
+        }
+        this._extractFiles(transfer.files);
+        this._preventStop(event);
+        this.mouseSobre.emit(false);
+    }
+
+    private _getTransference(event: any) {
+        return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+    }
+
+    private _extractFiles(archivosLista: FileList) {
+        for (const prop in Object.getOwnPropertyNames(archivosLista)) {
+            const temp = archivosLista[prop];
+            if (this._isFileUploadAvailable(temp)) {
+                const newFile = new FileItem(temp);
+                this.archivos.push(newFile);
+            }
+        }
+        console.log(this.archivos);
     }
 
     // Validaciones
